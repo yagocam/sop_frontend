71169108ce1e3ef0a27cbedd621de90477752499
yagocam <yagocam5@gmail.com>
2025-08-03 22:52:39 -0300

    removing unused app props

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,7 +2,6 @@
 
 import '@mantine/core/styles.css';
 import { ReactNode } from 'react';
-import type { AppProps } from 'next/app';
 import { MantineProvider, } from '@mantine/core';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '../store';
