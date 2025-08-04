22a7d16aa90c11f8f0affb2f01817ce31eb8f250
yagocam <yagocam5@gmail.com>
2025-08-03 22:50:57 -0300

    adding postcss

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,6 +2,7 @@
 
 import '@mantine/core/styles.css';
 import { ReactNode } from 'react';
+import type { AppProps } from 'next/app';
 import { MantineProvider, } from '@mantine/core';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '../store';
